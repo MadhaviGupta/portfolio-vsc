a26758f2653ff188e87e5eba037469294096eed4
MadhaviGupta <madhavigupta1225@gmail.com>
2023-06-26 14:14:29 +0530

    moved components to page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,5 @@
-"use client";
-import { useState } from "react";
-import Explorer from "./components/explorer";
-import Navbar from "./components/navbar";
-import Sidebar from "./components/sidebar";
 import "./globals.css";
 import { Manrope } from "next/font/google";
-import Page from "./page";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -15,15 +9,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [content, setContent] = useState("home");
   return (
     <html lang="en">
-      <body className={manrope.className}>
-        <Navbar/>
-        <Sidebar />
-        <Explorer content={content} setContent={setContent}/>
-        <Page content={content} setContent={setContent}/>
-        </body>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
-"use client"
+"use client";
 import Sidebar from "./components/sidebar";
 import Explorer from "./components/explorer";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import { useState } from "react";
-export default function Page({content, setContent}) {
+export default function Page() {
+  const [content, setContent] = useState("home");
 
   return (
     <main className="">
+      <Navbar />
       <div className="flex">
+        <Sidebar />
+        <Explorer content={content} setContent={setContent} />
         <Hero content={content} setContent={setContent} />
       </div>
     </main>
